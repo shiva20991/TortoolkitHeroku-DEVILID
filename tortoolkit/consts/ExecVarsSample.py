a2b27dae4d8b1d694d9d0a417fcd61655b04ac91
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2022-01-13 08:40:22 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -11,7 +11,7 @@ except:
         BOT_TOKEN = os.environ.get("BOT_TOKEN")
         BASE_URL_OF_BOT = os.environ.get("BASE_URL")
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = [1748487413,-587757607,-1001350892714]
+        ALD_USR = [-1001450720338]
         
         # Time to wait before edit message
         EDIT_SLEEP_SECS = 5
